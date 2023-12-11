1c72f47af4c2344ff2db4fdbdf3ea00796ec30ce
Anna-Kyra <anna-kyra.strik@hva.nl>
2023-12-11 11:31:51 +0100

    Frontend knop
    - veranderd achtergrondkleur van de body met class = "green"
    - De tekst toggled ook mee

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,21 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+let body = document.querySelector('body')
+
+frontend.addEventListener('click', bgcolor)
+
+function bgcolor() {
+  body.classList.toggle("green");
+  console.log("green")
+
+  if(this.innerHTML === "Frontend") {
+    this.innerHTML = "Groen";
+    console.log("groen_schrijf")
+  } else {
+    this.innerHTML = "Frontend";
+    console.log("frontend_schrijf")
+  }
+}
